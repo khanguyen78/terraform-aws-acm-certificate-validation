67c2820b304517fa1d9204d16dceee471f2717d7
Bao Nguyen <bnguyen@ctacorp.com>
2025-10-24 00:24:21 -0400

    some updates

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
-output "certificate_validation_status" {
-  description = "The status of the certificate validation."
-  value       = aws_acm_certificate_validation.this.status
-}
+#output "certificate_validation_status" {
+#  description = "The status of the certificate validation."
+#  value       = aws_acm_certificate_validation.this.status
+#}
 
 output "certificate_arn" {
   description = "The ARN of the validated ACM certificate."
